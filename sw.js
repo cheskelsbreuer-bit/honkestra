0712dd0f5638e665d75012010477e46b6a311bea
cheskelsbreuer-bit <cheskelsbreuer-bit@users.noreply.github.com>
2026-05-17 15:01:31 -0400

    Landing page at root, SVG car silhouettes, 25 new songs, category tabs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'honkestra-v1';
-const FILES = ['./', './index.html', './manifest.json'];
+const FILES = ['./', './index.html', './app.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
   self.skipWaiting();
