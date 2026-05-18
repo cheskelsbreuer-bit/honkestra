ec7f56cce64353e23abd10d6e3da14f5e6e82d19
cheskelsbreuer-bit <cheskelsbreuer-bit@users.noreply.github.com>
2026-05-17 21:56:31 -0400

    Revert SVG cars to emoji, remove dup practice mode, add surprise-me + sound test, fix cat filter, bump SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'honkestra-v1';
+const CACHE = 'honkestra-v2';
 const FILES = ['./', './index.html', './app.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
