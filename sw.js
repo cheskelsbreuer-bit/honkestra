799b15e45f9c37c2c7b7384ea51168155e379428
cheskelsbreuer-bit <cheskelsbreuer-bit@users.noreply.github.com>
2026-05-17 22:04:30 -0400

    Fix multi-phone bugs: global broadcast channel + radius selector + 4-digit-only code subscribe + lenient presence/lobby + debug strip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'honkestra-v2';
+const CACHE = 'honkestra-v3';
 const FILES = ['./', './index.html', './app.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
